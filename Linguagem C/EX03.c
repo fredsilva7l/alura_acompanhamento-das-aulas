b5a5e1574dd2a7327c113dda08611564af471028
Perkles <fredericojunior1995@gmail.com>
2022-03-19 20:27:50 -0300

    76de100

--- Linguagem C/EX03.c
+++ Linguagem C/EX03.c
@@ -3,24 +3,24 @@
 #include <stdlib.h>
 
 int main() {
-  int n, i;
+  int numero, i;
   printf("Digite a opcao\n1 - n = (++i) +i *(i+1)\n2 - n = (i++) +i *(i+1)\n3 - n = 1*(i+1) + (++i)\n4 - n = 1*(i+1) + (i++)\n", n);
   scanf("%d", & n);
 
-  if (n == 1) {
-    printf("O valor e:", n = (++i) + i * (i + 1));
+  if (numero == 1) {
+    printf("O valor e:", numero = (++i) + i * (i + 1));
   }
-  if (n == 2) {
-    printf("O valor e:", n = (i++) + i * (i + 1));
+  if (numero == 2) {
+    printf("O valor e:", numero = (i++) + i * (i + 1));
   }
-  if (n == 3) {
-    printf("O valor e:", n = 1 * (i + 1) + (++i));
+  if (numero == 3) {
+    printf("O valor e:", numero = 1 * (i + 1) + (++i));
   }
-  if (n == 4) {
-    printf("O valor e:", n = (++i) + i * (i + 1));
+  if (numero == 4) {
+    printf("O valor e:", numero = (++i) + i * (i + 1));
   }
-  if (n > 4) {
-    printf("Valor invalido:", n = 1 * (i + 1) + (i++));
+  if (numero > 4) {
+    printf("Valor invalido:", numero = 1 * (i + 1) + (i++));
   }
   return 0;
 }
