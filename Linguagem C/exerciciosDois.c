a8da52bf6e95247747610a0ee0861749fffbeb6c
Perkles <fredericojunior1995@gmail.com>
2022-03-21 14:58:54 -0300

    78de100

---
+++ Linguagem C/exerciciosDois.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int ano;
+    printf("Escreva ano que quer verificar \n");
+    scanf("%d", & ano);
+
+    if ((ano % 4 == 0 && !(ano % 100 == 0)) || ano % 400 ==0)
+    printf("ano bissexto");
+    else
+    printf("nao e ano bissexto");
+
+    return 0;
+}
