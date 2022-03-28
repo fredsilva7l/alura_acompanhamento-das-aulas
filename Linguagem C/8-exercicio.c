78bfaa75dffdf96ac3d65022936fd256098f7b2d
Perkles <fredericojunior1995@gmail.com>
2022-03-28 13:09:52 -0300

    85de100

---
+++ Linguagem C/8-exercicio.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int fatorial = 0, resultado = 1;
+
+    printf("Escreva qual numero quer calcular o fatorial \n", fatorial);
+    scanf("%d", & fatorial);
+
+    for (int i = 1; i < fatorial; i++) resultado += resultado * i;
+ 
+    printf("O valor do fatorial de %d e igual a %d", fatorial, resultado);
+
+    return 0;
+}
