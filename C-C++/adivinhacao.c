f0feaff2703f453319359e9e9b4500310e59b43f
Perkles <fredericojunior1995@gmail.com>
2022-04-06 14:54:56 -0300

    94de100

---
+++ C-C++/adivinhacao.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("***************************************** \n");
+    printf(" Bem vindo ao nosso jogo de adivinhacao! \n");
+    printf("***************************************** \n");
+    return 0;
+}
